469fa9d8b4e48d017c801dd9aabea0bd641b70b9
amr-amr <andrei.romascanu@mail.mcgill.ca>
2019-04-09 18:18:09 -0400

    fix: fixed bugs with data loader and environment

--- demo.py
+++ demo.py
@@ -1,15 +1,17 @@
 from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines.common.policies import MlpLstmPolicy, FeedForwardPolicy
+from stable_baselines.common.policies import MlpLstmPolicy, FeedForwardPolicy, LstmPolicy, CnnPolicy
 from gym.envs.registration import register
 from stable_baselines import DQN, A2C
 import gym
 import os
 import numpy as np
+from gym_summarizer.envs.extractive_env import RewardHelper
 
 best_mean_reward, n_steps = -np.inf, 0
 
 
 def callback(_locals, _globals):
+    # TODO: modify env RewardHelper to change reward between epochs (avoid recreating env for plotting reasons)
     global n_steps, best_mean_reward
     if (n_steps + 1) % 100 == 0:
         print(n_steps)
@@ -25,10 +27,39 @@ register(
 # Create log dir
 log_dir = "logs/gym"
 os.makedirs(log_dir, exist_ok=True)
-
 env_name = 'ExtractiveEnv-v0'
-env = gym.make(env_name)
+
+
+experiment_name = "A2C_rouge2_intermediate_0.0.2"
+env = gym.make(env_name, reward_helper=RewardHelper(reward_name='rouge-2', reward_type='f', is_terminal=False))
 env = DummyVecEnv([lambda: env])
-model = A2C(MlpLstmPolicy, env, tensorboard_log=f"../logs/tensorboard/{env_name}/",ent_coef=0.1, verbose=1)
-# model.learn(total_timesteps=1000, callback=callback)
-model.learn(total_timesteps=1000, callback=callback)
+# model = A2C(Policy, env,ent_coef=0.1, verbose=1)
+model = A2C(LstmPolicy, env, tensorboard_log=f"../logs/tensorboard/{experiment_name}/",ent_coef=0.1, verbose=1)
+model.learn(total_timesteps=100000, callback=callback)
+model.save(experiment_name)
+
+experiment_name = "A2C_rouge2_terminal_0.0.1"
+env = gym.make(env_name, reward_helper=RewardHelper(reward_name='rouge-2', reward_type='f', is_terminal=True))
+env = DummyVecEnv([lambda: env])
+# model = A2C(Policy, env, ent_coef=0.1, verbose=1)
+model = A2C(MlpLstmPolicy, env, tensorboard_log=f"../logs/tensorboard/{experiment_name}/",ent_coef=0.1, verbose=1)
+model.learn(total_timesteps=100000, callback=callback)
+model.save(experiment_name)
+
+"""
+0.0.0
+    Episode reward for terminal reward was always -0.4 (i.e. agent never selected sentences)
+    However, advantage went up to 0, indicating all actions equally bad...?
+        
+    Addendum: upon further inspection, the issue seems to have calling _get_obs() before updating state.
+    We address this in 0.0.1; 
+        - call _get_obs after state update
+        - no longer count invalid actions towards "sentences_written".
+        - enforce summary length of min(max_summary_len, article_len)
+    
+0.0.1
+
+0.0.2
+    LSTM without MLP
+    
+"""
--- gym_summarizer/envs/extractive_env.py
+++ gym_summarizer/envs/extractive_env.py
@@ -5,11 +5,14 @@ nltk.download('punkt')
 import numpy as np
 import pandas as pd
 import pickle
+import random  # TODO: set seed somewhere
 import tensorflow as tf
 import tensorflow_hub as hub
 
 from bert_serving.client import BertClient
-from typing import List, Dict
+from typing import List, Dict, NamedTuple, FrozenSet, Tuple
+
+from rouge import Rouge
 
 
 class SentEmbedder:
@@ -36,19 +39,26 @@ class SentEmbedder:
 
 class BertSentEmbedder(SentEmbedder):
     def __init__(self, model_dir="data/bert/uncased_L-12_H-768_A-12/", max_seq_len='NONE'):
-        self._init_bert_server(model_dir, max_seq_len)
-        self.bc = BertClient()
+        self.bc = self._init_bert_server(model_dir, max_seq_len)
         self.dim = 768
 
     def _init_bert_server(self, model_dir, max_seq_len):
-        from bert_serving.server.helper import get_args_parser
-        from bert_serving.server import BertServer
-        args = get_args_parser().parse_args(['-model_dir', model_dir,
-                                             '-max_seq_len', max_seq_len,
-                                             '-mask_cls_sep',
-                                             '-cpu'])
-        server = BertServer(args)
-        server.start()
+        try:
+            bc = BertClient()
+        except:
+            from bert_serving.server.helper import get_args_parser
+            from bert_serving.server import BertServer
+            args = get_args_parser().parse_args(['-model_dir', model_dir,
+                                                 '-max_seq_len', max_seq_len,
+                                                 '-mask_cls_sep',
+                                                 '-cpu',
+                                                 '-verbose'])
+            server = BertServer(args)
+            server.start()
+            bc = BertClient()
+        return bc
+        self.se_session = tf.Session()
+        self.se_session.run([tf.global_variables_initializer(), tf.tables_initializer()])
 
     def embed_article(self, sentences: List[str], max_len: int):
         embeddings = self.bc.encode(sentences)
@@ -83,9 +93,11 @@ class DataLoader:
 
 
 class Duc2007Loader(DataLoader):
-    def __init__(self, articles_path, summaries_path):
+    def __init__(self, articles_path, summaries_path, sent_embed_len, sent_embedder=BertSentEmbedder()):
         self.articles: pd.DataFrame = self.load_articles(articles_path)
         self.summaries: Dict = self.load_summaries(summaries_path)
+        self.sent_embed_len = sent_embed_len
+        self.sent_embedder = sent_embedder
 
     def load_articles(self, articles_path):
         with open(articles_path, 'rb') as f:
@@ -122,39 +134,238 @@ class Duc2007Loader(DataLoader):
         article = self.topic_articles[self.j].replace("\n", " ")
         summary = self.summaries.iloc[self.i]['Summary'].replace("\n", " ")
 
-        return article, summary
+        # tokenize article into sentences and sentence embeddings
+        embeddings = self.sent_embedder.embed_article(nltk.tokenize.sent_tokenize(article), self.sent_embed_len)
+
+        self.j += 1
+
+        return article, embeddings, summary
+
+
+class Duc2007PreEmbedLoader(DataLoader):
+    def __init__(self, articles_path, summaries_path, embeddings_path, sent_embed_len):
+        self.articles: pd.DataFrame = self.load_articles(articles_path)
+        self.summaries: Dict = self.load_summaries(summaries_path)
+        self.embeddings: Dict = self.load_summaries(embeddings_path)
+        self.sent_embed_len = sent_embed_len
+
+    def load_articles(self, articles_path):
+        with open(articles_path, 'rb') as f:
+            articles = pickle.load(f)
+        return articles
+
+    def load_summaries(self, summaries_path):
+        df = pd.read_pickle(summaries_path)
+        return df
+
+    def load_precomputed_embeddings(self, embeddings_path):
+        with open(embeddings_path, 'rb') as f:
+            embeddings = pickle.load(f)
+        return embeddings
+
+    def _pad_embeddings(self, embeddings):
+        padded = np.zeros((self.sent_embed_len, len(embeddings[0])), dtype=np.float32)
+        if len(embeddings) > self.sent_embed_len:
+            padded[:] = embeddings[:self.sent_embed_len]
+        else:
+            padded[:len(embeddings)] = embeddings
+        return padded
+
+    def __iter__(self):
+        self.i = 0
+        self.j = 0
+        topic = self.summaries.iloc[self.i]['Topic']
+        self.topic_articles = self.articles[topic]
+        self.topic_embeddings = self.embeddings[topic]
+        return self
+
+    def __next__(self):
+        """Yields article/summary pairs for a given topic.
+
+        Iterates through summaries (i) and through articles with same topic (j).
+
+        :return:
+        """
+        if self.i >= len(self.summaries):
+            raise StopIteration
+
+        # move to next summary when all articles for topic are passed
+        if self.j >= len(self.topic_articles):
+            self.i += 1
+            self.j = 0
+            self.topic_articles = self.articles[self.summaries.iloc[self.i]['Topic']]
+            self.topic_embeddings = self.embeddings[self.summaries.iloc[self.i]['Topic']]
+
+        article = self.topic_articles[self.j].replace("\n", " ")
+        embeddings = self._pad_embeddings(self.topic_embeddings[self.j])
+        summary = self.summaries.iloc[self.i]['Summary'].replace("\n", " ")
+
+        self.j += 1
+
+        return article, embeddings, summary
+
+
+class RewardSchedule(NamedTuple):
+    reward_name: str = 'rouge-1'  # 'rouge-1', 'rouge-2', 'rouge-l'
+    reward_type: str = 'f'  # f, r, p
+    start_epoch: int = 0
+    # end_epoch: int = None         # # TODO: args for finer-grained control over scheduling. Not implemented yet.
+    # start_episode: int = 0
+    # end_episode: int = None
+    # start_step: int = 0
+    # end_step: int = None
+
+
+# class UnhelpfulRewardHelper:
+#     """Helper class for shaping/scheduling reward in summarization env.
+#
+#     # TODO: revisit this for more complex scheduling if needed. Currently overdesigned af...
+#     Note:   This doesn't make sense in the context where scheduling happens across epochs,
+#             since we'll be reinstantiating the env each epoch.
+#             It's easier to just pass the reward type to the env for each epoch.
+#             However, this makes sense for per-episode or per-action scheduling.
+#
+#     Currently designed for three cases:
+#     - Stochastic reward:
+#         Randomly select reward for possible rouge scores.
+#     - Single reward:
+#         Always return same reward type.
+#     - Scheduled rewards:
+#
+#     """
+#     raise NotImplementedError
+#
+#     def __init__(self, schedules: List[RewardSchedule] = None, is_stochastic: bool = False):
+#         self.schedule = self._validate_schedules(schedules)
+#         self.is_stochastic = is_stochastic
+#         self.rouge = Rouge()
+#
+#         self.current_epoch = 0
+#         self.next_switch_epoch = 0
+#         self.current_reward_name = None
+#         self.current_reward_type = None
+#
+#     def _validate_schedules(self, schedules: List[RewardSchedule],
+#                             allowed_rewards: FrozenSet[str] = frozenset(['rouge-1', 'rouge-2', 'rouge-l']),
+#                             allowed_reward_types: FrozenSet[str] = frozenset(['f', 'r', 'p'])
+#                             ) -> List[Dict[int, Tuple[str, str]]]:
+#         """Sort reward schedules by start epoch and validate values.
+#
+#         """
+#         assert all(s.reward_name in allowed_rewards for s in schedules), \
+#             f"Invalid reward in schedule: allowed rewards are: {allowed_rewards}."
+#
+#         assert all(s.reward_type in allowed_reward_types for s in schedules), \
+#             f"Invalid reward type in schedule: allowed rewards are: {allowed_reward_types}."
+#
+#         assert len(s.start_epoch for s in schedules) == len(set(s.start_epoch for s in schedules)), \
+#             "Invalid start epochs in schedule: each reward must have a different start epoch."
+#
+#         schedule = {s.start_epoch: (s.reward_name, s.reward_type) for s in schedules}
+#
+#         assert 0 in schedule, "Invalid start epochs in schedule: must include start epoch of 0."
+#
+#         self.current_reward_name, self.current_reward_type = schedule[0]
+#         return schedule
+#
+#     def rouge_score(self, prediction, target):
+#         scores = self.rouge.get_scores(hyps=prediction, refs=target)
+#
+#         # stochastic case
+#         if self.is_stochastic:
+#             rewards = sum((list(s.values()) for s in scores.values()), [])  # collapse nested reward dict into list
+#             reward = random.choice(rewards)
+#
+#         # scheduled/constant case
+#         else:
+#             rouge_name, rouge_type = self._which_rouge_type()
+#             reward = scores[rouge_name][rouge_type]
+#
+#         return reward
+#
+#     def _which_rouge_type(self):
+#         if self.current_epoch in self.schedule:
+#             self.current_reward_name, self.current_reward_type = self.schedule[self.current_epoch]
+#         return self.current_reward_name, self.current_reward_type
+
+
+class RewardHelper:
+    def __init__(self, reward_name: str, reward_type: str, is_terminal=False, is_stochastic=False):
+        """
+
+        :param reward_name: ROUGE algorithm ('rouge-1', 'rouge-2', 'rouge-l')
+        :param reward_type: ROUGE type ('f', 'r', 'p') i.e. F1, Precision, Recall
+        :param is_terminal: Whether to return reward only on episode termination
+        :param is_stochastic: Whether to return random ROUGE algorithm/type score
+        """
+        assert reward_name in ['rouge-1', 'rouge-2', 'rouge-l']
+        assert reward_type in ['f', 'r', 'p']
+
+        self.reward_name = reward_name
+        self.reward_type = reward_type
+        self.is_terminal = is_terminal
+        self.is_stochastic = is_stochastic
+
+        self.reward_calculator = Rouge().get_scores
+
+    def get_reward(self, predicted: str, target: str, terminal: bool):
+        """Calculates reward (ROUGE) based on specified parameters.
+
+        :param predicted: Agent-generated summary.
+        :param target: Gold-standard reference summary.
+        :param terminal: Whether or not state is terminal (end of episode).
+        :return reward: ROUGE score
+        """
+        if self.is_terminal and terminal:
+            reward = 0
+        else:
+            rewards = self.reward_calculator(predicted, target)[0]
+            if self.is_stochastic:
+                reward = random.choice(sum((list(s.values()) for s in rewards.values()), []))
+            else:
+                reward = rewards[self.reward_name][self.reward_type]
+
+        return reward
 
 
 class ExtractiveEnv(gym.Env):
-    def __init__(self, data_loader: DataLoader = Duc2007Loader("data/dict-duc-2007-articles.pkl",
-                                                               "data/df-duc-2007-gold.pkl"),
+    def __init__(self, data_loader: DataLoader = Duc2007PreEmbedLoader("data/dict-duc-2007-articles.pkl",
+                                                                       "data/df-duc-2007-gold.pkl",
+                                                                       "data/dict-duc-2007-articles-embed.pkl",
+                                                                       250),
                  sent_embedder: SentEmbedder = BertSentEmbedder(),
-                 article_len: int = 20, summary_len: int = 4):
+                 reward_helper: RewardHelper = RewardHelper(reward_name='rouge-2', reward_type='f'),
+                 article_dim: int = 50, summary_dim: int = 10,
+                 max_summary_len: int = 250):
         # helpers
         self.data_loader = data_loader
+        self.data_loader.sent_embed_len = article_dim  # TODO: clean up this hackiness for embedding padding...
         self.data_iterator = iter(self.data_loader)  # yields article(s), summary
         self.sent_embedder = sent_embedder
+        self.reward_helper = reward_helper
 
         # dimensions
-        self.article_len = article_len
-        self.summary_len = summary_len
+        self.article_dim = article_dim
+        self.summary_dim = summary_dim
         self.sent_embed_dim = sent_embedder.dim
 
         # env attributes
-        self.action_space = gym.spaces.Discrete(self.article_len - 1)  # article sentence index selection
+        self.action_space = gym.spaces.Discrete(self.article_dim - 1)  # article sentence index selection
         self.observation_space = gym.spaces.Box(
             low=-1,
             high=1,
-            shape=(self.article_len + self.summary_len, self.sent_embed_dim),
+            shape=(self.article_dim + self.summary_dim, self.sent_embed_dim, 1),
             dtype='float32',
         )
 
         # current episode information
         self.article: List[str] = None
-        self.article_tensor: np.ndarray = np.zeros((self.article_len, self.sent_embed_dim))
+        self.article_tensor: np.ndarray = np.zeros((self.article_dim, self.sent_embed_dim))
         self.summary_pred: str = None  # summary output so far
-        self.summary_tensor: np.ndarray = np.zeros((self.summary_len, self.sent_embed_dim))
+        self.summary_tensor: np.ndarray = np.zeros((self.summary_dim, self.sent_embed_dim))
         self.summary_target: str = None  # gold standard summary
+        self.summary_len = 0
+        self.max_summary_len = max_summary_len
         self.sentences_written = 0
         self.rouge_score = 0
         self.actions: set = set()
@@ -162,29 +373,28 @@ class ExtractiveEnv(gym.Env):
         self.reset()
 
     def reset(self):
-        """
-        - clear everything
-        - get new summary
-
-        :return:
-        """
         # yield next article/summary
         try:
-            self.article, self.summary_target = next(self.data_iterator)
+            self.article, self.article_tensor, self.summary_target = next(self.data_iterator)
         except StopIteration:
             print("Every article has been loaded, restarting data_iterator")
-            self.article, self.summary_target = next(self.data_iterator)
+            self.data_iterator = iter(self.data_loader)
+            self.article, self.article_tensor, self.summary_target = next(self.data_iterator)
 
-        # tokenize article into sentences and update article_tensor with sentence embeddings
+        # tokenize article into sentences
         self.article = nltk.tokenize.sent_tokenize(self.article)
-        self.article_tensor = self.sent_embedder.embed_article(self.article, self.article_len)
 
         # reset everything else
         self.summary_pred = ""
-        self.summary_tensor = np.zeros((self.summary_len, self.sent_embed_dim))
+        self.summary_tensor = np.zeros((self.summary_dim, self.sent_embed_dim))
+        self.summary_len = 0
         self.sentences_written = 0
         self.rouge_score = 0
         self.actions = set()
+
+        # reshape tensor (x,y) -> (x,y,1)
+        self.article_tensor = self.article_tensor.reshape(self.article_dim, self.sent_embed_dim, 1)
+        self.summary_tensor = self.summary_tensor.reshape(self.summary_dim, self.sent_embed_dim, 1)
 
         return self._get_obs()
 
@@ -193,26 +403,45 @@ class ExtractiveEnv(gym.Env):
 
         if action in self.actions or action >= len(self.article):
             reward = -0.1
+            obs = self._get_obs()
 
         else:
-            self.summary_pred += self.article[action]
+            sentence = self.article[action]
+            self.summary_pred += sentence
             self.summary_tensor[self.sentences_written] = self.article_tensor[action]
             self.sentences_written += 1
+            self.summary_len += len(sentence.split(" "))
             self.actions.add(action)
-            reward = self._get_reward()
 
-        obs = self._get_obs()
+            if (self.summary_len >= self.max_summary_len) or \
+                    (self.sentences_written >= len(self.article)) or \
+                    (self.sentences_written >= self.summary_dim):
+                self.summary_pred = " ".join(self.summary_pred.split(" ")[:self.max_summary_len])
+                done = True
+                reward = self._get_reward(done)
+                obs = self._get_obs()
 
-        if self.sentences_written >= self.summary_len:
-            done = True
-        return obs, reward, done, {"summary": self.summary_pred}
+                # debugging
+                print(self.summary_pred)
+                if self.summary_len < self.max_summary_len:
+                    print(self.summary_len)
+
+                self.reset()
+
+                return obs, reward, done, {}
+
+            reward = self._get_reward(done)
+            obs = self._get_obs()
+
+        return obs, reward, done, {}
 
     def _get_obs(self):
         return np.concatenate([self.article_tensor, self.summary_tensor], axis=0)
 
-    def _get_reward(self):
+    def _get_reward(self, done):
         prev_rouge = self.rouge_score
-        self.rouge_score = self._lcs_rouge(target=self.summary_target, predicted=self.summary_pred)
+        self.rouge_score = self.reward_helper.get_reward(target=self.summary_target, predicted=self.summary_pred,
+                                                         terminal=done)
         return self.rouge_score - prev_rouge
 
     def _lcs_rouge(self, target, predicted):
